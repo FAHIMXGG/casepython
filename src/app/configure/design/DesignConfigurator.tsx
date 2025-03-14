cd82f80f3536b59f2453c55ea1fa6dea0848c2a9
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2025-03-14 11:57:54 +0600

    model dropdown

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -8,12 +8,19 @@ import { Rnd } from "react-rnd";
 import { RadioGroup } from '@headlessui/react'
 import { useState } from "react";
 import { color } from "framer-motion";
-import { COLORS } from "@/validators/option-validator";
+import { COLORS, MODELS } from "@/validators/option-validator";
 import { Label } from "@/components/ui/label";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Check, ChevronsUpDown } from "lucide-react";
 
 // bg-blue-800 border-blue-800
 // bg-zinc-900 border-zinc-900
 // bg-rose-900 border-rose-900
+// bg-[#FFE7E7] border-[#FFE7E7]
+// bg-[#F3F5E7] border-[#F3F5E7]
+// bg-[#FFFBE2] border-[#FFFBE2]
+
 
 interface DesignConfiguratorProps {
     configID: string;
@@ -24,9 +31,10 @@ const DesignConfigurator = ({ configID, imageUrl, imageDimension }: DesignConfig
 
     const [options, setOptions] = useState<{
         color: (typeof COLORS)[number]
+        model: (typeof MODELS.options)[number]
     }>({
         color: COLORS[0],
-
+        model: MODELS.options[0],
     })
     return (
         <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
@@ -36,7 +44,7 @@ const DesignConfigurator = ({ configID, imageUrl, imageDimension }: DesignConfig
                         <NextImage fill alt="phone image" src='/phone-template.png' className="pointer-events-none z-50 select-none" />
                     </AspectRatio>
                     <div className="absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]" />
-                    <div className={cn('absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]', `bg-zinc-950`)} />
+                    <div className={cn('absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]', `bg-${options.color.tw}`)} />
                 </div>
                 <Rnd default={{
                     x: 0,
@@ -66,23 +74,57 @@ const DesignConfigurator = ({ configID, imageUrl, imageDimension }: DesignConfig
                         <h2 className="tracking-tight font-bold text-3xl">Customize your Case</h2>
                         <div className="w-full h-px bg-zinc-200 my-6" />
                         <div className="relative mt-4 h-full flex flex-col justify-between">
-                            <RadioGroup value={options.color} onChange={(value) => setOptions((prev) => ({ ...prev, color: value }))}>
-                                <Label>Color: {options.color.label}</Label>
-                                <div className="mt-3 flex items-center space-x-3">
-                                    {COLORS.map((color) => (
-                                        <RadioGroup.Option key={color.label}
-                                            value={color}
-                                            className={({ active, checked }) => cn("relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent", {
-                                                [`border-${color.tw}`]: active || checked,
-                                            })}
-                                        >
-                                            <span className={cn(`bg-${color.tw}`, "h-5 w-5 rounded-full border border-black ")}></span>
+                            <div className="flex flex-col gap-6">
+                                <RadioGroup value={options.color} onChange={(value) => setOptions((prev) => ({ ...prev, color: value }))}>
+                                    <Label>Color: {options.color.label}</Label>
+                                    <div className="mt-3 flex items-center space-x-3">
+                                        {COLORS.map((color) => (
+                                            <RadioGroup.Option key={color.label}
+                                                value={color}
+                                                className={({ active, checked }) => cn("relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent", {
+                                                    [`border-${color.tw}`]: active || checked,
+                                                })}
+                                            >
+                                                <span className={cn(`bg-${color.tw}`, "h-5 w-5 rounded-full border border-black ")}></span>
+                                            </RadioGroup.Option>
 
-                                        </RadioGroup.Option>
-
-                                    ))}
+                                        ))}
+                                    </div>
+                                </RadioGroup>
+                                <div className="relative flex flex-col gap-3 w-full">
+                                    <Label>Model</Label>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button
+                                                variant='outline'
+                                                role="combobox"
+                                                className="w-full justify-between"
+                                            >
+                                                {options.model.label}
+                                                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            {
+                                                MODELS.options.map((model) => (
+                                                    <DropdownMenuItem
+                                                        key={model.label}
+                                                        className={cn('flex text-sm gap-1 items-center p-1.5 cursor-default hover:bg-zinc-100', {
+                                                            "bg-zinc-100": model.label === options.model.label
+                                                        })}
+                                                        onClick={() => {
+                                                            setOptions((prev) => ({ ...prev, model }));
+                                                        }}
+                                                    >
+                                                        <Check className={cn("mr-2 hj-4 w-4", model.label === options.model.label ? 'opacity-100' : 'opacity-0')} />
+                                                        {model.label}
+                                                    </DropdownMenuItem>
+                                                ))
+                                            }
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
                                 </div>
-                            </RadioGroup>
+                            </div>
                         </div>
                     </div>
                 </ScrollArea>
--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -10,4 +10,36 @@ export const COLORS = [
     {
         label: 'Rose', value: 'rose', tw: 'rose-900'
     },
+    {
+        label: 'Pink', value: 'pink', tw: '[#FFE7E7]'
+    },
+    {
+        label: 'Green', value: 'green', tw: '[#F3F5E7]'
+    },
+    {
+        label: 'Yellow', value: 'yellow', tw: '[#FFFBE2]'
+    },
 ] as const
+
+
+export const MODELS = {
+    name: 'models',
+    options: [
+        {
+            label: 'iPhone 14 Pro',
+            value: 'iPhone 14 Pro',
+        },
+        {
+            label: 'iPhone 14 Pro Max',
+            value: 'iPhone 14 Pro Max',
+        },
+        {
+            label: 'iPhone 14',
+            value: 'iPhone 14',
+        },
+        {
+            label: 'iPhone 14 Plus',
+            value: 'iPhone 14 Plus',
+        },
+    ]
+} as const

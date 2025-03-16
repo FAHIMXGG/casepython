8bf3a001530b8470a6dcfd561487f35188e6a46b
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2025-03-16 17:09:27 +0600

    material and finish opt

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -8,7 +8,7 @@ import { Rnd } from "react-rnd";
 import { RadioGroup } from '@headlessui/react'
 import { useState } from "react";
 import { color } from "framer-motion";
-import { COLORS, MODELS } from "@/validators/option-validator";
+import { COLORS, FINISHES, MATERIALS, MODELS } from "@/validators/option-validator";
 import { Label } from "@/components/ui/label";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
@@ -32,9 +32,13 @@ const DesignConfigurator = ({ configID, imageUrl, imageDimension }: DesignConfig
     const [options, setOptions] = useState<{
         color: (typeof COLORS)[number]
         model: (typeof MODELS.options)[number]
+        material: (typeof MATERIALS.options)[number]
+        finish: (typeof FINISHES.options)[number]
     }>({
         color: COLORS[0],
         model: MODELS.options[0],
+        material: MATERIALS.options[0],
+        finish: FINISHES.options[0],
     })
     return (
         <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
@@ -124,6 +128,43 @@ const DesignConfigurator = ({ configID, imageUrl, imageDimension }: DesignConfig
                                         </DropdownMenuContent>
                                     </DropdownMenu>
                                 </div>
+
+                                {[MATERIALS, FINISHES].map(({ name, options: selectableOptions }) => (
+                                    <RadioGroup
+                                        key={name}
+                                        value={options[name]}
+                                        onChange={(val) => {
+                                            setOptions((prev) => ({ ...prev, [name]: val }));
+                                        }}
+                                    >
+                                        <Label>{name.slice(0, 1).toUpperCase() + name.slice(1)}</Label>
+                                        <div className="mt-3 space-y-4">
+                                            {selectableOptions.map((option) => (
+                                                <RadioGroup.Option
+                                                    key={option.value}
+                                                    value={option}
+                                                    className={({ active, checked }) => cn('relative block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between', {
+                                                        "border-primary": active || checked,
+                                                    }
+                                                    )
+                                                    }>
+                                                    <span className="flex items-center">
+                                                        <span className="flex flex-col text-sm">
+                                                            <RadioGroup.Label className='font-medium text-gray-900' as="span">
+                                                                {option.label}
+                                                            </RadioGroup.Label>
+                                                            {option.description ? <RadioGroup.Description as="span"
+                                                                className='text-gray-500'
+                                                            >
+                                                                <span className="block sm:inline">{option.description}</span>
+                                                            </RadioGroup.Description> : null}
+                                                        </span>
+                                                    </span>
+                                                </RadioGroup.Option>
+                                            ))}
+                                        </div>
+                                    </RadioGroup>
+                                ))}
                             </div>
                         </div>
                     </div>
--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -1,3 +1,4 @@
+import { PRODUCT_PRICE } from "@/config/products"
 
 
 export const COLORS = [
@@ -40,6 +41,42 @@ export const MODELS = {
         {
             label: 'iPhone 14 Plus',
             value: 'iPhone 14 Plus',
+        },
+    ]
+} as const
+
+export const MATERIALS = {
+    name: 'material',
+    options: [
+        {
+            label: 'Silicone',
+            value: 'silicone',
+            description: undefined,
+            price: PRODUCT_PRICE.material.silicone,
+        },
+        {
+            label: 'Soft Polycarbonate',
+            value: 'polycarbonate',
+            description: "Scratch-resistant and durable",
+            price: PRODUCT_PRICE.material.polycarbonate,
+        },
+    ]
+} as const
+
+export const FINISHES = {
+    name: 'finish',
+    options: [
+        {
+            label: 'Smooth Finish',
+            value: 'smooth',
+            description: undefined,
+            price: PRODUCT_PRICE.finish.smooth,
+        },
+        {
+            label: 'Textured Finish',
+            value: 'textured',
+            description: "Soft grippy texture",
+            price: PRODUCT_PRICE.finish.textured,
         },
     ]
 } as const

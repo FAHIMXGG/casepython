54341a181e3b96eb1752102542898450cd45be26
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2025-03-18 15:30:36 +0600

    img save state p1

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -6,14 +6,16 @@ import { cn, formatPrice } from "@/lib/utils";
 import NextImage from "next/image";
 import { Rnd } from "react-rnd";
 import { RadioGroup } from '@headlessui/react'
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { color } from "framer-motion";
 import { COLORS, FINISHES, MATERIALS, MODELS } from "@/validators/option-validator";
 import { Label } from "@/components/ui/label";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { ArrowRight, Check, ChevronsUpDown } from "lucide-react";
+import { ArrowRight, Check, ChevronsUpDown, Weight } from "lucide-react";
 import { BASE_PRICE } from "@/config/products";
+import { useUploadThing } from "@/lib/uploadthing";
+import { toast } from "sonner"
 
 // bg-blue-800 border-blue-800
 // bg-zinc-900 border-zinc-900
@@ -29,7 +31,7 @@ interface DesignConfiguratorProps {
     imageDimension: { width: number; height: number }
 }
 const DesignConfigurator = ({ configID, imageUrl, imageDimension }: DesignConfiguratorProps) => {
-
+    
     const [options, setOptions] = useState<{
         color: (typeof COLORS)[number]
         model: (typeof MODELS.options)[number]
@@ -41,22 +43,107 @@ const DesignConfigurator = ({ configID, imageUrl, imageDimension }: DesignConfig
         material: MATERIALS.options[0],
         finish: FINISHES.options[0],
     })
+
+    const [renderedDimensions, setRenderedDimensions] = useState({
+        width: imageDimension.width / 4,
+        height: imageDimension.height / 4,
+    })
+
+    const [renderedPosition, setRenderedPosition] = useState({
+        x: 150,
+        y: 205,
+    })
+
+    //console.log(renderedDimensions, renderedPosition)
+    const phoneCaseRef = useRef<HTMLDivElement>(null)
+    const containerRef = useRef<HTMLDivElement>(null)
+
+    const {startUpload} = useUploadThing('imageUploader')
+
+    async function saveConfiguration () {
+        try {
+            const { left: caseLeft, top: caseTop, width, height  } = phoneCaseRef.current!.getBoundingClientRect()
+
+            const {left:containerLeft, top: containerTop, } = containerRef.current!.getBoundingClientRect()
+
+            const leftOffset = caseLeft - containerLeft
+            const topOffset = caseTop - containerTop
+
+            const actualX = renderedPosition.x - leftOffset
+            const actualY = renderedPosition.y - topOffset
+
+            const canvas = document.createElement('canvas')
+            canvas.width = width
+            canvas.height = height
+            const ctx = canvas.getContext('2d')
+
+            const userImage = new Image()
+            userImage.crossOrigin = 'anonymous'
+            userImage.src =imageUrl
+            await new Promise((resolve) => (userImage.onload = resolve))
+
+            ctx?.drawImage(
+                userImage,
+                actualX,
+                actualY,
+                renderedDimensions.width,
+                renderedDimensions.height
+            )
+
+            const base64 = canvas.toDataURL()
+            console.log(base64)
+            const base64Data = base64.split(',')[1]
+
+            const blob = base64ToBlob(base64Data, "image/png")
+            const file = new File([blob], 'filename.png', { type: 'image/png' })
+
+            await startUpload([file], {configId:configID})
+        } catch (error) {
+            toast("Error uploading image", {
+                description: (<p className="text-white font-bold text-center"> There was an error uploading your image. Please try again.</p>),
+                style: { backgroundColor: "red", color: "white" }
+            })
+        }
+
+        function base64ToBlob(base64: string, mineType: string) {
+            const byteCharacters = atob(base64)
+            const byteNumbers = new Array(byteCharacters.length)
+            for(let i = 0 ; i < byteCharacters.length; i++) {
+                byteNumbers[i] = byteCharacters.charCodeAt(i)
+            }
+            const byteArray = new Uint8Array(byteNumbers)
+            return new Blob([byteArray], { type: mineType })
+        }
+    }
+
     return (
         <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
-            <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+            <div ref={containerRef} className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
                 <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
-                    <AspectRatio ratio={896 / 1831} className="pointer-events-none relative z-50 aspect-[896/1831]">
+                    <AspectRatio ref={phoneCaseRef} ratio={896 / 1831} className="pointer-events-none relative z-50 aspect-[896/1831]">
                         <NextImage fill alt="phone image" src='/phone-template.png' className="pointer-events-none z-50 select-none" />
                     </AspectRatio>
                     <div className="absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]" />
                     <div className={cn('absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]', `bg-${options.color.tw}`)} />
                 </div>
                 <Rnd default={{
-                    x: 0,
-                    y: 0,
+                    x: 300,
+                    y: 200,
                     width: imageDimension.width / 4,
                     height: imageDimension.height / 4
                 }}
+                    onResizeStop={(_, __, ref, ___, { x, y }) => { 
+                        setRenderedDimensions({
+                            height: parseInt(ref.style.height.slice(0,2)),
+                            width: parseInt(ref.style.width.slice(0,2))
+                        })
+
+                        setRenderedPosition({ x, y })
+                     }}
+                     onDragStop={(_, data) =>{
+                        const { x, y } = data
+                        setRenderedPosition({ x, y })
+                     }}
                     className="absolute z-30 border-[1px] border-primary"
                     lockAspectRatio
                     resizeHandleComponent={{
@@ -183,7 +270,7 @@ const DesignConfigurator = ({ configID, imageUrl, imageDimension }: DesignConfig
                             <p className="font-medium whitespace-nowrap">
                                 {formatPrice((BASE_PRICE + options.finish.price + options.material.price) / 100)}
                             </p>
-                            <Button size='sm' className='w-full'> 
+                            <Button onClick={() => saveConfiguration()} size='sm' className='w-full'>
                                 Continue
                                 <ArrowRight className="h-4 w-4 ml-1.5 inline" />
                             </Button>

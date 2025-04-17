7f4f5aa194e35bf77cc5b31f2cb4751a3efaab83
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2025-04-17 21:31:11 +0600

    auth callback

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -3,24 +3,41 @@
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { getAuthStatus } from "./action";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
 
 const page = () => {
-    const [configID, setConfigID] = useState<string | null>(null)
+    const [configId, setConfigID] = useState<string | null>(null)
+    const router = useRouter()
 
     useEffect(() => {
         const configurationId = localStorage.getItem('configurationId')
         if (configurationId) setConfigID(configurationId)
     }, [])
 
-    const {} = useQuery({
+    const {data} = useQuery({
         queryKey: ['auth-callback'],
         queryFn: async () => await getAuthStatus(),
         retry: true,
         retryDelay: 500
     })
+
+    if(data?.success) {
+        if (configId) {
+            localStorage.removeItem('configurationId')
+            router.push(`/configure/preview?id=${configId}`)
+        } else {
+            router.push('/configure/upload')
+        }
+    }
+
     return (
-        <div>
-            
+        <div className="w-full mt-24 flex justify-center">
+            <div className="flex flex-col items-center gap-2">
+                <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+                <h3 className="font-semibold text-xl">Logging you in...</h3>
+                <p>You will be redirected automatically.</p>
+            </div>
         </div>
     );
 };

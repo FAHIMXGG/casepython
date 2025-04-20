ad41a1a2d68824b0e46464c69743a9edb77ec949
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2025-04-20 15:33:06 +0600

    thank you

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -3,12 +3,16 @@
 import React from 'react';
 import { getPaymentStatus } from './actions';
 import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'next/navigation';
 
 const ThankYou = () => {
-
-    const {} = useQuery({
+    const searchParams = useSearchParams()
+    const orderId = searchParams.get('orderId') || ''
+    const {data} = useQuery({
         queryKey: ['payment-status'],
-        queryFn: async () => await getPaymentStatus(),
+        queryFn: async () => await getPaymentStatus({orderId}),
+        retry: true,
+        retryDelay: 500,
     })
     return (
         <div>

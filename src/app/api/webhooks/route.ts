fe99518c1866681d9160f541313c8b7a1505e5e3
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2025-04-20 09:43:14 +0600

    webhook update

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -6,72 +6,77 @@ import Stripe from "stripe"
 
 export async function POST(req: Request) {
     try {
-        const body = await req.text()
-        const signature = headers().get('stripe-signature')
-
-        if(!signature) {
-            return new Response('Invalid signature', {status: 400})
+      const body = await req.text()
+      const signature = headers().get('stripe-signature')
+  
+      if (!signature) {
+        return new Response('Invalid signature', { status: 400 })
+      }
+  
+      const event = stripe.webhooks.constructEvent(
+        body,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET!
+      )
+  
+      if (event.type === 'checkout.session.completed') {
+        if (!event.data.object.customer_details?.email) {
+          throw new Error('Missing user email')
         }
-
-        const event = stripe.webhooks.constructEvent(
-            body, 
-            signature, 
-            process.env.STRIPE_WEBHOOK_SECRET!
-        )
-        
-        if (event.type === 'checkout.session.completed') {
-            if(!event.data.object.customer_details?.email) {
-                throw new Error('Missing customer email')
-            }
-
-            const session = event.data.object as Stripe.Checkout.Session
-
-            const {userId, orderId} = session.metadata || {
-                userId: null,
-                orderId: null,
-            }
-
-            if(!userId || !orderId) {
-                throw new Error('Invalid request metadata')
-            }
-
-            const billingAddress = session.customer_details!.address
-            const shippingAddress = session.shipping_details!.address
-
-            await db.order.update({
-                where: {
-                    id: orderId,
-                },
-                data: {
-                    isPaid: true,
-                    shippingAddress: {
-                        create: {
-                            name: session.customer_details!.name!,
-                            city: shippingAddress!.city!,
-                            country: shippingAddress!.country!,
-                            zip: shippingAddress!.postal_code!,
-                            street: shippingAddress!.line1!,
-                            state: shippingAddress!.state!,
-                            phoneNumber: session.customer_details!.phone!
-                        }
-                    },
-                    billingAddress: {
-                        create: {
-                            name: session.customer_details!.name!,
-                            city: billingAddress!.city!,
-                            country: billingAddress!.country!,
-                            zip: billingAddress!.postal_code!,
-                            street: billingAddress!.line1!,
-                            state: billingAddress!.state!,
-                            phoneNumber: session.customer_details!.phone!
-                        }
-                    },
-                }
-            })
+  
+        const session = event.data.object as Stripe.Checkout.Session
+  
+        const { userId, orderId } = session.metadata || {
+          userId: null,
+          orderId: null,
         }
-        return NextResponse.json({result:event, ok:true})
+  
+        if (!userId || !orderId) {
+          throw new Error('Invalid request metadata')
+        }
+  
+        const billingAddress = session.customer_details!.address
+        const shippingAddress = session.shipping_details!.address
+  
+        const updatedOrder = await db.order.update({
+          where: {
+            id: orderId,
+          },
+          data: {
+            isPaid: true,
+            shippingAddress: {
+              create: {
+                name: session.customer_details!.name!,
+                city: shippingAddress!.city!,
+                country: shippingAddress!.country!,
+                zip: shippingAddress!.postal_code!,
+                street: shippingAddress!.line1!,
+                state: shippingAddress!.state,
+                phoneNumber: session.customer_details!.phone!,
+              },
+            },
+            billingAddress: {
+              create: {
+                name: session.customer_details!.name!,
+                city: billingAddress!.city!,
+                country: billingAddress!.country!,
+                zip: billingAddress!.postal_code!,
+                street: billingAddress!.line1!,
+                state: billingAddress!.state,
+                phoneNumber: session.customer_details!.phone!,
+              },
+            },
+          },
+        })
+      }
+  
+      return NextResponse.json({ result: event, ok: true })
     } catch (err) {
-        console.error(err)
-        return NextResponse.json({result:null, ok:false, error:err}, {status: 500})
+      console.error(err)
+  
+      return NextResponse.json(
+        { message: 'Something went wrong', ok: false },
+        { status: 500 }
+      )
     }
-}
+  }

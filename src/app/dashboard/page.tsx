4b1972a80a160a7c27ca370cedbd849bbad598bb
Ahasanul Haque <ahasanulhaquefahim1@gmail.com>
2025-04-25 17:23:11 +0600

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,27 +1,130 @@
-import { db } from '@/db';
-import React from 'react';
+import { db } from "@/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { notFound } from "next/navigation";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import React from "react";
+import { cn, formatPrice } from "@/lib/utils";
+import { OrderStatus } from "@prisma/client";
+import PhonePreview from "@/components/PhonePreview";
+import Phone from "@/components/Phone";
+import { COLORS } from "@/validators/option-validator";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+const LABEL_MAP: Record<OrderStatus, string> = {
+  awaiting_shipment: "Awaiting Shipment",
+  fulfilled: "Fulfilled",
+  shipped: "Shipped",
+};
 
 const Page = async () => {
-    const orders = await db.order.findMany({
-        where: {
-          isPaid: true,
-          createdAt: {
-            gte: new Date(new Date().setDate(new Date().getDate() - 7)),
-          },
+  const user = await currentUser();
+
+  if (!user?.id) {
+    return notFound();
+  }
+
+  const orders = await db.order.findMany({
+    where: {
+      userId: user.id,
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      user: true,
+      shippingAddress: true,
+      configuration: {
+        select: {
+          croppedImageUrl: true,
+          model: true,
+          color: true,
         },
-        orderBy: {
-          createdAt: "desc",
-        },
-        include: {
-          user: true,
-          shippingAddress: true,
-        },
-      });
-    return (
-        <div>
-            
+      },
+    },
+  });
+  const tw = COLORS.find(
+    (supportedColor) => supportedColor.value === orders[0]?.configuration?.color
+  )?.tw;
+  console.log(orders);
+
+  return (
+    <div className="flex min-h-screen w-full bg-muted/40 ">
+      <div className="max-w-7xl w-full mx-auto flex flex-col sm:gap-4 sm:py-4">
+        <div className="flex flex-col gap-16">
+          <h1 className="text-4xl font-bold tracking-tight lg:mt-28">Your orders</h1>
+
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Item</TableHead>
+                <TableHead className="hidden sm:table-cell">Name</TableHead>
+                <TableHead className="hidden sm:table-cell">
+                  Purchase date
+                </TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+                <TableHead className="text-right">Track</TableHead>
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {orders.map((order) => (
+                <TableRow key={order.id} className="bg-accent">
+                  <TableCell>
+                    <div className="w-10">
+                      <Phone
+                        className={cn(
+                          `bg-${tw}`,
+                          "max-w-[150px] md:max-w-full"
+                        )}
+                        imgSrc={order.configuration.croppedImageUrl!}
+                      />
+                    </div>
+                    <div className="hidden text-sm text-muted-foreground md:inline"></div>
+                  </TableCell>
+                  <TableCell className="hidden sm:table-cell">
+                    {/* {LABEL_MAP[order.status]} */}
+                    {order.configuration.model} - {order.configuration.color}
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    {order.createdAt.toLocaleDateString()}
+                  </TableCell>
+                  <TableCell className="text-right">
+                    {formatPrice(order.amount)}
+                  </TableCell>
+                  <TableCell className="text-right ">
+                    <div className="flex justify-end">
+                      <Link
+                        href="/configure/upload"
+                        className={buttonVariants({
+                          size: "sm",
+                          className: "hidden sm:flex items-center gap-1 w-20",
+                        })}
+                      >
+                        Track
+                      </Link>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Page;
